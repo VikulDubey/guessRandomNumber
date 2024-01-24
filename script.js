9827e52f85cfd15882180c0d5c0bb97bd4784fd0
Vikul Dubey <vikuldubey43@gmail.com>
2024-01-24 19:32:33 -0400

    add restart button & some stying

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const form = document.querySelector("form");
 const result = document.querySelector("#result");
 const previousGuesses = document.querySelector(".previousGuesses");
 const remainingGuesses = document.querySelector(".remainingGuesses");
+const resultPara = document.querySelector(".resultPara");
 const emptyArray = [];
 let attempts = 10;
 let invalidAttempts = 1;
@@ -32,12 +33,12 @@ form.addEventListener("submit", (e) => {
     inputFieldValue < 1 ||
     inputFieldValue > 100
   ) {
-    result.innerHTML = "Enter a valid value.";
+    resultPara.innerHTML = "Enter a valid value.";
     invalidAttempts++;
     console.log(invalidAttempts);
     if (invalidAttempts > 3) {
       Object.freeze(emptyArray);
-      result.innerHTML = "You entered multiple invalid inputs.";
+      resultPara.innerHTML = "You entered multiple invalid inputs.";
       setTimeout(() => {
         alert("Refresh the Page! To Play Again");
       }, 2000);
@@ -45,35 +46,39 @@ form.addEventListener("submit", (e) => {
   }
   // When you guessed right number.
   else if (inputFieldValue === randomNumber) {
-    result.innerHTML = `You guessed right Number: ${inputFieldValue}`;
+    resultPara.innerHTML = `You guessed right Number: ${inputFieldValue}`;
     remainingGuesses.innerHTML = `Remaining Guesses : ${attempts}`;
     remainingGuesses.classList.add("style");
     Object.freeze(emptyArray);
-    setTimeout(() => {
-      alert(`Refresh Now for new Game!`);
-    }, 1000);
+    const btn = document.createElement("button");
+    btn.className = "reloadBtn";
+    btn.innerHTML = "Refresh now";
+    result.appendChild(btn);
+    btn.addEventListener("click", () => {
+      window.location.reload();
+    });
   }
   // When attempts finished.
   else if (attempts == 0) {
     Object.freeze(emptyArray);
 
-    result.innerHTML = `You have finished your attempts`;
+    resultPara.innerHTML = `You have finished your attempts`;
   }
   // A complete else block for not equal to random number.
   else {
     // If you guessed lower number.
     if (inputFieldValue > randomNumber) {
-      result.innerHTML = "You guessed higher than the random number";
+      resultPara.innerHTML = "You guessed higher than the random number";
       inputField.value = "";
     }
     // If you guessed higher Number.
     else if (inputFieldValue < randomNumber) {
-      result.innerHTML = "You guessed lower than the random number";
+      resultPara.innerHTML = "You guessed lower than the random number";
       inputField.value = "";
     }
     // If you guessed Wrong Number.
     else {
-      result.innerHTML = "Wrong Guessed!";
+      resultPara.innerHTML = "Wrong Guessed!";
       inputField.value = "";
     }
     // If you guessed wrong the attempts will be decrease.
